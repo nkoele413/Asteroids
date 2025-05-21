0b4318a15faf5bfd52e298978904be5be8d08ba8
nkoele413 <nkoele413@gmail.com>
2025-05-20 19:39:44 -0600

    added left and right control input

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ def main():
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    while 1 > 0:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
